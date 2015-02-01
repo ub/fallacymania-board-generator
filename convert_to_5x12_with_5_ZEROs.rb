d73388a84655fd346646902b193fdb94c8b400f8
Yuri Baranov <baranovu@gmail.com>
2015-02-01 21:19:12 +0300

    removed

--- convert_to_5x12_with_5_ZEROs.rb
+++ convert_to_5x12_with_5_ZEROs.rb
@@ -7,33 +7,19 @@ image = ChunkyPNG::Image.from_file('rhetological-fallacies.png')
 puts image.dimension.height
 puts image.dimension.width
 
-sample_target = ChunkyPNG::Image.new(200,200, ChunkyPNG::Color::WHITE)
-
-buffer = image.crop(0,0,100,100)
-sample_target.replace!(buffer)
-
-sample_target.save('sample-out.png')
+puts '='*10
 
 source_grid = MasterGrid.new
 
 
 
-target_grid_4x14 = MasterCompatibleGrid.new(4,14)
-puts '-'*10
-puts target_grid_4x14.canvas_width
-puts target_grid_4x14.canvas_height
-puts '-'*10
 target_grid_5x11 = MasterCompatibleGrid.new(5,11)
-puts target_grid_5x11.canvas_width
-puts target_grid_5x11.canvas_height
-puts '-'*10
+
 target_grid_5x12 = MasterCompatibleGrid.new(5,12)
 puts target_grid_5x12.canvas_width
 puts target_grid_5x12.canvas_height
 
 
-target4x15 =  ChunkyPNG::Image.new(target_grid_4x14.canvas_width, target_grid_4x14.canvas_height, ChunkyPNG::Color::WHITE)
-target5x11 =  ChunkyPNG::Image.new(target_grid_5x11.canvas_width, target_grid_5x11.canvas_height, ChunkyPNG::Color::WHITE)
 target5x12 =  ChunkyPNG::Image.new(target_grid_5x12.canvas_width, target_grid_5x12.canvas_height, ChunkyPNG::Color::WHITE)
 
 
@@ -43,14 +29,6 @@ File::open('rhetological-fallacies.mgdm') do |f|
 end
 
 
-# target_iterator = target_grid_4x14.each_cell_topleft
-# source_grid.each_cell_topleft(@master_description.each_cell_index) do | y, x |
-#   buffer = image.crop(x, y, source_grid.cell_width, source_grid.cell_height)
-#   t_y, t_x = target_iterator.next
-#   target4x15.replace!(buffer, t_x, t_y)
-# end
-#
-# target4x15.save('almost_square.png')
 
 
 target_iterator=target_grid_5x11.each_cell_topleft
